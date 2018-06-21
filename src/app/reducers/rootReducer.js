6c5bd7e72842fe764660d5c51995a7731daa5e87
Mike <munkyu0813@gmail.com>
2018-06-21 05:14:46 -0400

    connect events state to view

--- src/app/reducers/rootReducer.js
+++ src/app/reducers/rootReducer.js
@@ -1,7 +1,9 @@
 import { combineReducers } from "redux";
 
+import eventReducer from "../../features/event/eventReducer";
+
 const rootReducer = combineReducers({
-  test: testReducer
+  events: eventReducer
 });
 
 export default rootReducer;
