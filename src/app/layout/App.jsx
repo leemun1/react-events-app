45d4bdb55903da0fc66d0d9e932be06aae69c6ea
Mike <munkyu0813@gmail.com>
2018-06-20 21:03:15 -0400

    add user related components and configure routing

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,17 +1,42 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import { Container } from "semantic-ui-react";
-import EventDashboard from "../../features/event/EventDashboard/EventDashboard";
+import { Route, Switch } from "react-router-dom";
 import NavBar from "../../features/nav/NavBar/NavBar";
+import HomePage from "../../features/home/HomePage";
+import EventDashboard from "../../features/event/EventDashboard/EventDashboard";
+import EventForm from "../../features/event/EventForm/EventForm";
+import SettingsDashboard from "../../features/user/Settings/SettingsDashboard";
+import UserDetailedPage from "../../features/user/UserDetailed/UserDetailedPage";
+import PeopleDashboard from "../../features/user/PeopleDashboard/PeopleDashboard";
+import EventDetailedPage from "../../features/event/EventDetailed/EventDetailedPage";
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <NavBar />
-        <Container className="main">
-          <EventDashboard />
-        </Container>
-      </div>
+      <Fragment>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+        </Switch>
+
+        <Route
+          path="/(.+)"
+          render={() => (
+            <Fragment>
+              <NavBar />
+              <Container className="main">
+                <Switch>
+                  <Route path="/events" component={EventDashboard} />
+                  <Route path="/event/:id" component={EventDetailedPage} />
+                  <Route path="/people" component={PeopleDashboard} />
+                  <Route path="/profile/:id" component={UserDetailedPage} />
+                  <Route path="/settings" component={SettingsDashboard} />
+                  <Route path="/createEvent" component={EventForm} />
+                </Switch>
+              </Container>
+            </Fragment>
+          )}
+        />
+      </Fragment>
     );
   }
 }
