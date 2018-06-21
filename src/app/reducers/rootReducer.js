7d2a415fcf31bd0ee8ef3e5ff116cada69e53cb2
Mike <munkyu0813@gmail.com>
2018-06-21 04:44:59 -0400

    setup redux

---
+++ src/app/reducers/rootReducer.js
@@ -0,0 +1,7 @@
+import { combineReducers } from "redux";
+
+const rootReducer = combineReducers({
+  test: testReducer
+});
+
+export default rootReducer;
---
+++ src/app/store/configureStore.js
@@ -0,0 +1,27 @@
+import { createStore, applyMiddleware } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import rootReducer from "../reducers/rootReducer";
+
+export const configureStore = preloadedState => {
+  // handle middlewares
+  const middlewares = [];
+  const middlewareEnhancer = applyMiddleware(...middlewares);
+
+  // put together enhancers
+  const storeEnhancers = [middlewareEnhancer];
+  const composedEnhancer = composeWithDevTools(...storeEnhancers);
+
+  // create store
+  const store = createStore(rootReducer, preloadedState, composedEnhancer);
+
+  // hot reloading for redux
+  if (process.env.NODE_ENV !== "production") {
+    if (module.hot) {
+      module.hot.accept("../reducers/rootReducer", () => {
+        const newRootReducer = require("../reducers/rootReducer").default;
+        store.replaceReducer(newRootReducer);
+      });
+    }
+  }
+  return store;
+};
