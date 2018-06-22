108036dc96fd96f0063592c4c383d56da98184cd
Mike <munkyu0813@gmail.com>
2018-06-22 01:07:56 -0400

    implement custom form with validations with redux-form

--- src/app/reducers/rootReducer.js
+++ src/app/reducers/rootReducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
+import { reducer as FormReducer } from "redux-form";
 
 import eventReducer from "../../features/event/eventReducer";
 
 const rootReducer = combineReducers({
+  form: FormReducer,
   events: eventReducer
 });
 
