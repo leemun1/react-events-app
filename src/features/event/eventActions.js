b51f2e1e5df02b80efa39011add0adc5388ef632
Mike <munkyu0813@gmail.com>
2018-06-22 18:33:25 -0400

    add toastr to show informing messages

--- src/features/event/eventActions.js
+++ src/features/event/eventActions.js
@@ -1,3 +1,5 @@
+import { toastr } from "react-redux-toastr";
+
 import {
   CREATE_EVENT,
   UPDATE_EVENT,
@@ -21,19 +23,28 @@ export const fetchEvents = events => {
 };
 
 export const createEvent = event => {
-  return {
-    type: CREATE_EVENT,
-    payload: {
-      event
+  return async dispatch => {
+    try {
+      dispatch({
+        type: CREATE_EVENT,
+        payload: {
+          event
+        }
+      });
+      toastr.success("Success!", "Event has been created.");
+    } catch (error) {
+      toastr.error("Oops..!", "Something went wrong.");
     }
   };
 };
 
 export const updateEvent = event => {
-  return {
-    type: UPDATE_EVENT,
-    payload: {
-      event
+  return async dispatch => {
+    try {
+      dispatch({ type: UPDATE_EVENT, payload: { event } });
+      toastr.success("Success!", "Event has been updated.");
+    } catch (error) {
+      toastr.error("Oops..!", "Something went wrong.");
     }
   };
 };
