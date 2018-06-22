6f173d583dc9fb314783f1403582f759c7189289
Mike <munkyu0813@gmail.com>
2018-06-22 19:03:13 -0400

    format dates using date-fns and add palceholder component for showing event activities

--- src/features/event/EventDashboard/EventDashboard.jsx
+++ src/features/event/EventDashboard/EventDashboard.jsx
@@ -5,6 +5,7 @@ import { Grid } from "semantic-ui-react";
 import EventList from "../EventList/EventList";
 import { deleteEvent } from "../eventActions";
 import LoadingComponent from "../../../app/layout/LoadingComponent";
+import EventActivity from "../EventActivity/EventActivity";
 
 const mapStateToProps = state => ({
   events: state.events,
@@ -29,7 +30,9 @@ class EventDashboard extends Component {
         <Grid.Column width={10}>
           <EventList events={events} deleteEvent={this.handleDeleteEvent} />
         </Grid.Column>
-        <Grid.Column width={6} />
+        <Grid.Column width={6}>
+          <EventActivity />
+        </Grid.Column>
       </Grid>
     );
   }
--- src/features/event/EventDetailed/EventDetailedInfo.jsx
+++ src/features/event/EventDetailed/EventDetailedInfo.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import format from "date-fns/format";
 import { Segment, Grid, Icon, Button } from "semantic-ui-react";
 import EventDetailedMap from "./EventDetailedMap";
 
@@ -6,6 +7,10 @@ class EventDetailedInfo extends Component {
   state = {
     showMap: false
   };
+
+  componentWillUnmount() {
+    this.setState({ showMap: false });
+  }
 
   showMapToggle = () => {
     this.setState(prevState => ({
@@ -33,7 +38,10 @@ class EventDetailedInfo extends Component {
               <Icon name="calendar" size="large" color="teal" />
             </Grid.Column>
             <Grid.Column width={15}>
-              <span>{event.date}</span>
+              <span>
+                {format(event.date, "dddd Do MMM")} at{" "}
+                {format(event.date, "h:mm A")}
+              </span>
             </Grid.Column>
           </Grid>
         </Segment>
